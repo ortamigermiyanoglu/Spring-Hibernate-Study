36fb3e49314ad808f080019c54a7ae80b4d48c90
Semir Umut Kurt <fatih.ek96@hotmail.com>
2019-11-19 22:23:36 +0300

    update

---
+++ src/main/java/com/sumutella/app/UpdateWithWithoutQuery.java
@@ -0,0 +1,34 @@
+package com.sumutella.app;
+
+import com.sumutella.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+/**
+ * @author sumutella
+ * @time 10:05 PM
+ * @since 11/19/2019, Tue
+ */
+public class UpdateWithWithoutQuery {
+
+
+    public static void main(String[] args) {
+        SessionFactory sessionFactory =new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Student.class).buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+
+        //Student student = session.get(Student.class, 2);
+        //student.setFullName("Ahmet Faruk Gebzeli");
+
+        session.createQuery("update Student set fullName='Ahmet Gebzeli' where id=2").executeUpdate();
+
+        session.getTransaction().commit();
+        sessionFactory.close();
+
+
+    }
+
+}
